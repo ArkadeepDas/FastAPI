94a2915f2336287454bbe852c0358516d340c863
ArkadeepDas <arkadeep.das8@gmail.com>
2023-05-24 11:35:53 +0530

    Multiple Options and Hidden Query

--- 5_StringValidation_QueryParameters.py
+++ 5_StringValidation_QueryParameters.py
@@ -9,7 +9,7 @@ app = FastAPI()
 ###########################################################
 
 # Now we want to add some validation in query parameter
-# Query parameter maxmimum 10 character
+# Query parameter maxmimum 10 character, minimum 3 character
 # So here we use Query()
 # We can add many parameters, min_length, max_length, regex
 # Here the default query is 'FixedQuery' and we remove the None, but we can set any value
@@ -32,3 +32,23 @@ async def read_item(q:str = Query(..., min_length = 3, max_length = 10)):
     if q:
         result.update({'q': q})
     return result
+
+# Let's see how we can add multiple options in single parameter
+# Now it will take multiple input
+@app.get('/list_items')
+# Here it's('q') a optional parameter
+# Now if we want to change the name in url from q to something then we add parameter 'alias'
+async def list_item(q:list[str] = Query(['Hello'], alias='jod')):
+    result = {'item id' : [{'item_id':'Foo'}, {'item_id':'Bar'}]}
+    if q:
+        result.update({'q': q})
+    return result
+
+# Let's see the hidden query
+# Here the Query parameter don't need any value but if we add something then it will show
+@app.get('/hidden_query')
+async def hidden_q(hidden_query:str | None = Query(None, include_in_schema = False)):
+    if hidden_query:
+        return {'Hidden Query':'Hidden Query present '+ hidden_query}
+    else:
+        return{'Hidden Query':'No data found'}
